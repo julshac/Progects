f3f4f028274899a9df79ea6c52f5eed33e2f0d64
julshac <julia_luxury@yahoo.com>
2015-03-26 22:27:22 +0300

    Numerology

---
+++ Module2 (2).vb
@@ -0,0 +1,41 @@
+﻿Module Module2
+
+    'Автор: Шац Юлия.
+    '********************************************************************************************************************************************
+    'Редакция от 28.11.2013.
+    '********************************************************************************************************************************************
+    'Высчитывает сумму цифр исходного числа, а затем сумму цифр полученной суммы, до тех пор, пока в ответе не получится значение из одной цифры.
+    '********************************************************************************************************************************************
+
+    Sub Main()
+        Dim figure As Integer
+
+        Console.Write("Введите число: ")
+        figure = Console.ReadLine()
+
+        Console.WriteLine("Полученное число: {0}", SumNumbers(figure))
+        Console.ReadLine()
+
+    End Sub
+
+    'Функция выподянет суммирование цифр в исходном и последующих числах.
+    Function SumNumbers(ByVal value As Integer) As Integer
+        Dim result As Integer
+        Dim curValue, Sum As Integer
+
+        If value > 0 Then
+            curValue = value
+            Do While curValue > 0
+                Sum = Sum + curValue Mod 10
+                curValue = curValue \ 10
+            Loop
+            If Sum > 9 Then
+                result = SumNumbers(Sum)
+            Else
+                result = Sum
+            End If
+        End If
+
+        Return result
+    End Function
+End Module
