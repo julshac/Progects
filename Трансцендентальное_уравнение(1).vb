5bbd71d728388d26ed50a55aff539b2144d20722
julshac <julia_luxury@yahoo.com>
2015-03-26 22:51:29 +0300

    Трансцендентальное уравнение

---
+++ Трансцендентальное_уравнение(1).vb
@@ -0,0 +1,67 @@
+﻿'Программист Бобров А.В.
+Imports System.Console
+Imports System.Math
+Module Module2
+    Sub main()
+        Title = "Математическая задача"
+        Dim a, b, e, x0, x1, x2 As Double
+        WriteLine("F(x)=tan(x) + (1 / 3) * tan(x) ^ 3 + (1 / 5) * tan(x) ^ 5 - (1 / 3)")
+        Write("Введите начало интервала: ")
+        a = ReadLine()
+        Write("Введите конец интервала: ")
+        b = ReadLine()
+        Write("Введите точность: ")
+        e = ReadLine()
+
+        'Первый случай'
+        If F(a) * Fpr(a) > 0 Then
+            x0 = b
+            x1 = x0 - (F(x0) * (a - x0)) / (F(a) - F(x0))
+            x2 = (x0 * (F(x1) - x1 * F(x0))) / (F(x1) - F(x0))
+            While Abs(x2 - x1) >= e
+                x0 = x1
+                x1 = x2
+                x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            End While
+        Else
+            x0 = a
+            x1 = x0 - (F(x0) * (x0 - b)) / (F(x0) - F(b))
+            x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            While Abs(x2 - x1) >= e
+                x0 = x1
+                x1 = x2
+                x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            End While
+        End If
+
+        'Второй случай'
+        If F(b) * Fpr(b) > 0 Then
+            x0 = a
+            x1 = x0 - (F(x0) * (b - x0)) / (F(b) - F(x0))
+            x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            While Abs(x2 - x1) >= e
+                x0 = x1
+                x1 = x2
+                x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            End While
+        Else
+            x0 = b
+            x1 = x0 - (F(x0) * (x0 - a)) / (F(x0) - F(a))
+            x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            While Abs(x2 - x1) >= e
+                x0 = x1
+                x1 = x2
+                x2 = x1 - (F(x1) * (x0 - x1)) / (F(x0) - F(x1))
+            End While
+        End If
+        WriteLine("x = {0:f3}", x2)
+        ReadKey()
+    End Sub
+    Function F(ByVal x As Double) As Double
+        Return Tan(x) + (1 / 3) * Tan(x) ^ 3 + (1 / 5) * Tan(x) ^ 5 - (1 / 3)
+    End Function
+    Function Fpr(ByVal x As Double) As Double
+        Return 2 * Tan(x) ^ 5 / Cos(x) ^ 2 + 4 * Tan(x) ^ 3 / Cos(x) ^ 4 - 2 * Tan(x) ^ 3 / Cos(x) ^ 2 + 2 * Tan(x) / Cos(x) ^ 2 - 2 * Tan(x) / Cos(x) ^ 4
+    End Function
+
+End Module
---
+++ Трансцендентальное_уравнение.vb
@@ -0,0 +1,49 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim value1, value2, value3, a, b, e, f, x As Single
+        Dim count As Integer
+
+        Console.WriteLine("F(x)=tan(x) + (1 / 3) * tan(x) ^ 3 + (1 / 5) * tan(x) ^ 5 - (1 / 3)")
+        Console.WriteLine("Введите начало интервала: a= ")
+        a = Console.ReadLine()
+
+        Console.WriteLine("Введите конец интервала: b= ")
+        b = Console.ReadLine()
+
+        Console.WriteLine("Введите точность: e= ")
+        e = Console.ReadLine()
+
+        value1 = Formula(a)
+        value2 = Formula(b)
+        value3 = Formula(f)
+
+        If (value1 And value2) < 0 Then
+            count = 1
+            Do
+                f = (a + b) / 2
+                If value1 * value2 <= 0 Then
+                    b = f
+                Else
+                    a = f
+                End If
+                count += 1
+            Loop Until b - a < e
+            x = (a + b) / 2
+        Else
+            x = a - 1
+        End If
+        If x = a - 1 Then
+            Console.WriteLine("Корней нет на данном интервале")
+        Else
+            Console.WriteLine("x = {0}, n = {1}", x, count)
+        End If
+        Console.ReadLine()
+
+    End Sub
+    Function Formula(ByVal sinx As Single) As Single
+        Dim y As Single
+        y = Math.Tan(sinx) + (1 / 3) * Math.Tan(sinx) ^ 3 + (1 / 5) * Math.Tan(sinx) ^ 5 - (1 / 3)
+        Return y
+    End Function
+End Module
